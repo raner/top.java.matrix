42027b8a50730d0aa69f76215c9a926ac8d11207
Mirko Raner <mirko@raner.ws>
2017-06-15 00:35:42 -0700

    implemented naive matrix multiplication

--- src/main/java/top/java/matrix/naive/NaiveMatrix.java
+++ src/main/java/top/java/matrix/naive/NaiveMatrix.java
@@ -1,5 +1,8 @@
 package top.java.matrix.naive;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 import top.java.matrix.Dimension;
 import top.java.matrix.Matrix;
 import top.java.matrix.util.RawFloatMatrix;
@@ -9,6 +12,13 @@ public class NaiveMatrix<ROWS extends Dimension, COLUMNS extends Dimension> impl
     private int rows;
     private int columns;
     private float[] matrix;
+
+    public NaiveMatrix(int rows, int columns, float[] matrix)
+    {
+        this.rows = rows;
+        this.columns = columns;
+        this.matrix = matrix;
+    }
 
     public NaiveMatrix(RawFloatMatrix matrix)
     {
@@ -20,8 +30,32 @@ public class NaiveMatrix<ROWS extends Dimension, COLUMNS extends Dimension> impl
     @Override
     public <DIMENSION extends Dimension> Matrix<ROWS, DIMENSION> times(Matrix<COLUMNS, DIMENSION> rightHandSide)
     {
-        // TODO Auto-generated method stub
-        return null;
+        // Multiplication example:
+        //
+        // ROWS X COLUMNS   COLUMNS X DIMENSION     ROWS x DIMENSION
+        //      3 x 5         5 x 2                      3 x 2
+        //
+        //                   ⎡p q⎤
+        //  ⎡a b c d e⎤     ⎢r s⎥       ⎡ap+br+ct+dv+ex   aq+bs+cu+dw+ey⎤
+        //  ⎢f g h i j⎥  X  ⎢t u⎥   =   ⎢fp+gr+ht+iv+jx   fq+gs+hu+iw+jy⎥
+        //  ⎣k l m n o⎦     ⎢v w⎥       ⎣kp+lr+mt+nv+ox   kq+ls+mu+nw+yo⎦
+        //                   ⎣x y⎦
+        //
+        int dimension = rightHandSide.getColumns();
+        float[] result = new float[rows*dimension];
+        for (int x = 0; x < dimension; x++) // columns
+        {
+            for (int y = 0; y < rows; y++) // rows
+            {
+                int sum = 0;
+                for (int z = 0; z < columns; z++)
+                {
+                    sum += matrix[y + z*rows] * rightHandSide.at(z, x);
+                }
+                result[y + x*rows] = sum;
+            }
+        }
+        return new NaiveMatrix<>(rows, dimension, result);
     }
 
     @Override
@@ -46,5 +80,25 @@ public class NaiveMatrix<ROWS extends Dimension, COLUMNS extends Dimension> impl
     public int getColumns()
     {
         return columns;
+    }
+
+    @Override
+    public boolean equals(Object other)
+    {
+        if (other instanceof NaiveMatrix)
+        {
+            NaiveMatrix<?, ?> otherMatrix = (NaiveMatrix<?, ?>)other;
+            if (rows == otherMatrix.getRows() && columns == otherMatrix.getColumns())
+            {
+                return Arrays.equals(otherMatrix.matrix, matrix);
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash(rows, columns, Arrays.hashCode(matrix));
     }
 }
--- src/test/java/top/java/matrix/naive/NaiveMatrixTest.java
+++ src/test/java/top/java/matrix/naive/NaiveMatrixTest.java
@@ -9,18 +9,25 @@ import top.java.matrix.Dimension;
 import top.java.matrix.Matrix;
 import top.java.matrix.util.OctaveFloatBinaryReader;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class NaiveMatrixTest<M extends Dimension>
 {
     private OctaveFloatBinaryReader reader = new OctaveFloatBinaryReader();
+    private Matrix<M, M> A;
     private Matrix<M, M> B;
+    private Matrix<M, M> thousand;
+    private Matrix<M, M> squared;
     private float δ = 1E-6F;
 
     @Before
     public void initializeMatrices() throws IOException
     {
         // TODO: load only once!
+        A = new NaiveMatrix<>(reader.readFloatBinaryMatrix(path("MatrixA.float.bin")));
         B = new NaiveMatrix<>(reader.readFloatBinaryMatrix(path("MatrixB.float.bin")));
+        thousand = new NaiveMatrix<>(reader.readFloatBinaryMatrix(path("Thousand.float.bin")));
+        squared = new NaiveMatrix<>(reader.readFloatBinaryMatrix(path("Squared.float.bin")));
     }
 
     @Test
@@ -40,7 +47,47 @@ public class NaiveMatrixTest<M extends Dimension>
     {
         assertEquals(3106F, B.at(1,  0), δ);
     }
-    
+
+    @Test
+    public void testEqualsEqual()
+    {
+        assertEquals(A, A);
+    }
+
+    @Test
+    public void testEqualsNotEqual()
+    {
+        assertNotEquals(A, B);
+    }
+
+    @Test
+    public void testSmallMultiplication3x3()
+    {
+        final Matrix<M, M> X = new NaiveMatrix<>(3, 3, new float[] {8, 5, 2, 1, 9, 7, 3, 0, 4});
+        final Matrix<M, M> Y = new NaiveMatrix<>(3, 3, new float[] {8, 3, 4, 1, 5, 9, 6, 7, 2});
+        Matrix<M, M> expected = new NaiveMatrix<>(3, 3, new float[] {79, 67, 53, 40, 50, 73, 61, 93, 69});
+        Matrix<M, M> result = X.times(Y);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testSmallMultiplication3x5by5x2()
+    {
+        final Matrix<M, M> X = new NaiveMatrix<>(3, 5, new float[] {1, 6, 11, 2, 7, 12, 3, 8, 13, 4, 9, 14, 5, 10, 15});
+        final Matrix<M, M> Y = new NaiveMatrix<>(5, 2, new float[] {1, 3, 5, 7, 9, 2, 4, 6, 8, 10});
+        Matrix<M, M> expected = new NaiveMatrix<>(3, 2, new float[] {95, 220, 345, 110, 260, 410});
+        Matrix<M, M> result = X.times(Y);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testLargeMultiplication1000x1000()
+    {
+        Matrix<M, M> expected = squared;
+        Matrix<M, M> result = thousand.times(thousand);
+        assertEquals(expected, result);
+    }
+
     private Path path(String name)
     {
         return new File("src/test/resources/" + name).getAbsoluteFile().toPath();
